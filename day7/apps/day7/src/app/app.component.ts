d427954eb4776c6d254d86bac0b1c2ba1cc4d606
Phan Kim Tuân <tuanpk@rikkeisoft.com>
2021-11-11 08:17:49 +0700

    add

--- day7/apps/day7/src/app/app.component.ts
+++ day7/apps/day7/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { interval, Observable } from 'rxjs';
+export interface typeUserName {
+  name: string;
+  age: number;
+}
 @Component({
   selector: 'day7-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'day7';
+  title?:string = 'day7';
+  currentDate:Date = new Date();
+  fullname?:string = "phan tuan rikkeisoft";
+  userInfo:typeUserName = {
+    name: 'tuanphan',
+    age: 23
+  }
+  interval$:Observable<number> = interval(1000);
 }
